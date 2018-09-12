92816867034766457253aeeafa286684d8563c2f
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-09-12 10:25:39 +0200

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
+Widget helloRectangle() {
+  return Container(
+    color: Colors.greenAccent
+    ,
+  );
+}
+
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: true,
-    home: Scaffold(
-      appBar: AppBar(
-        elevation: 0.5,
-        title: Text('Demo Staless Widget'),
-      ),
-      body: Container(
-        color: Colors.greenAccent,
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0.5,
+          title: Text('Demo Staless Widget'),
+        ),
+        body: helloRectangle(),
       ),
     ),
-  ));
+  );
 }
